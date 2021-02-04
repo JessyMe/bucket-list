54ed8324bfe22c106d1f1bbd72fdfe6f517578e1
JessyMe <jessy.mercer@gmail.com>
2021-02-04 19:50:28 +0100

    CRUD
    Affichage de la liste et ses détails

--- bucket-list/src/Controller/IdeaController.php
+++ bucket-list/src/Controller/IdeaController.php
@@ -4,8 +4,13 @@
 namespace App\Controller;
 
 
+
+use App\Entity\Idea;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 /**
  * @Route ("/idea");
@@ -13,20 +18,56 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IdeaController extends AbstractController
 {
+
+    /**
+     * @Route ("/new");
+     */
+    public function add(EntityManagerInterface $em)
+    {
+    /* $idea = new Idea();
+     $idea->setTitle("Lire à la recherche du temps perdu, en entier, en français.");
+     $idea->setDescription("Rien n'est plus grand que la culture, voyager dans votre esprit et apprenez à vous élever.");
+     $idea->setAuthor("MrGaston_prof");
+     $idea->setIsPublished(true);
+     $idea->setDateCreated(new \DateTime());
+
+     $em->persist($idea);
+
+     $idea2 = new Idea();
+     $idea2->setTitle("Vivre sur une île deserte");
+     $idea2->setDescription("Et qu'on arrête enfin de me casser les pieds.");
+     $idea2->setAuthor("HanSoloDuGhetto");
+     $idea2->setIsPublished(true);
+     $idea2->setDateCreated(new \DateTime());
+
+     $em->persist($idea2);
+
+     $em->flush();
+
+     return $this->render('idea/list.html.twig');
+*/
+    }
+
     /**
      * @Route("/", name = "idea_list");
      */
     public function list()
     {
-
-        return $this->render("idea/list.html.twig");
+        $repoIdea = $this->getDoctrine()->getRepository(Idea::class);
+        $ideas = $repoIdea->findBy([], ["dateCreated" => "DESC" ]);
+        return $this->render("idea/list.html.twig", [
+            "ideas"=>$ideas
+        ]);
     }
     /**
-     * @Route("/detail/{id}", requirements={"id"="\d+"}, name = "idea_detail");
+     * @Route("/detail/{id}", requirements={"id"="\d+"}, name = "idea_detail",
+     *     methods={"GET"})
      */
-    public function detail($id = null)
+    public function detail($id = null, Request $request)
     {
-        return $this->render("idea/detail.html.twig");
+        $repoIdea = $this->getDoctrine()->getRepository(Idea::class);
+        $idea = $repoIdea->find($id);
+        return $this->render("idea/details.html.twig", ["idea" => $idea]);
     }
 
 }
---
+++ bucket-list/src/Entity/Idea.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IdeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IdeeRepository::class)
+ */
+class Idea
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column (type="string", length=250)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column (type="text", nullable=true )
+     */
+    private $description;
+
+    /**
+     * @ORM\Column (type="string", length=100)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column (type="boolean")
+     */
+    private $isPublished;
+
+    /**
+     * @ORM\Column (type="datetime")
+     */
+    private $dateCreated;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsPublished()
+    {
+        return $this->isPublished;
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished): void
+    {
+        $this->isPublished = $isPublished;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param mixed $dateCreated
+     */
+    public function setDateCreated($dateCreated): void
+    {
+        $this->dateCreated = $dateCreated;
+    }
+
+
+}
